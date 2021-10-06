bae0c940000fcaa16522c91d8ae0ece4731a5697
aldoortiz128 <aldoortiz128@gmail.com>
2021-10-06 19:08:19 +0000

    Finished

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 //1
-function absoluteValue(){
+function absoluteValue(num){
+     if (Number.isInteger(num) && num < 0)
+  {return num * -1}
   
+   else if (Number.isInteger(num))
+  {return num}
+ 
+  else if (num != Number.isInteger(num))
+  {return "Null"}
 }
 
+console.log(absoluteValue("diamond"));
+
+//Class solution
+
+//
+//if (typeof (int)==="number") Type of will check to see if the variable is an integer.
+//    
+//
 
 
 
